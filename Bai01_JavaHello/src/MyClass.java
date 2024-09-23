116fc887ace243aecea39cc2b2c9928b824b4ae3
HaoMixua <haotranvipqwe15@gmail.com>
2024-09-23 20:31:29 +0700

    first commit - done

---
+++ Bai01_JavaHello/src/MyClass.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class MyClass {
+
+	/**
+	 * @access public
+	 * @param array
+	 * @return null
+	 */
+	public static void main(String[] args) {
+		// Khai báo biến
+		
+		int a = 2, b=5;
+		int c = a+b;
+		
+		System.out.println("Tổng của a:"+a+" và b:"+b+" là: "+c);
+	}
+
+}
