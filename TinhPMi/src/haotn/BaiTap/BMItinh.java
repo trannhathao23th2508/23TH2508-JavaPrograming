7dde736811a0b7be33fafa59cbdffa6d50ebb2b1
trannhathao23th2508 <haotranvipqwe15@gmail.com>
2024-09-24 20:25:07 +0700

    hanh thanh

--- TinhPMi/src/haotn/BaiTap/BMItinh.java
+++ TinhPMi/src/haotn/BaiTap/BMItinh.java
@@ -1,5 +1,7 @@
 package haotn.BaiTap;
 
+import java.util.Scanner;
+
 public class BMItinh {
 
 	public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class BMItinh {
 		Scanner banPhim = new Scanner (System.in);
 		//nhập Họ Tên 
 		System.out.print("họ tên :");
-		String hoTen = banPhim.nextline();
+		String hoTen= banPhim.nextLine();
 		//nhap chieu cao
 	System.out.print("chiều cao :");
 	double chieuCao =banPhim.nextDouble();
@@ -17,8 +19,22 @@ public class BMItinh {
 	
 	double bmi=canNang/(chieuCao*chieuCao);
 	
-	system.out.println("bạn"+hoTen);
-	system.out.println("bmi của bạn "+bmi);
+	System.out.println("bạn"+hoTen);
+	System.out.println("bmi của bạn "+bmi);
+	
+	if (bmi<18.5)
+		System.out.println("thiếu cân ");
+	else if (bmi<25)
+		System.out.println("cân nặng bình thường  ");
+	else if (bmi<30)
+		System.out.println("thừa cân , tiền béo phì ");
+	else if (bmi<35)
+		System.out.println("béo phì độ 1 ");
+	else if (bmi<40)
+		System.out.println("béo phì độ 2 ");
+	else System.out.println("béo phì dộ 3 (bệnh lý)");
+	
+	
 	
 		
 		
