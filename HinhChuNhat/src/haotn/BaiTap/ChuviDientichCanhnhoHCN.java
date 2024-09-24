87858f023fdbb549db5a3bc60c59e9950391c76b
trannhathao23th2508 <haotranvipqwe15@gmail.com>
2024-09-24 22:14:36 +0700

    nhap lieu

---
+++ HinhChuNhat/src/haotn/BaiTap/ChuviDientichCanhnhoHCN.java
@@ -0,0 +1,28 @@
+package haotn.BaiTap;
+
+import java.util.Scanner;
+
+public class ChuviDientichCanhnhoHCN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 class HinhChuNhat {
+		    public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+
+		        // Nhập chiều dài và chiều rộng
+		        System.out.print("Nhập chiều dài: ");
+		        double chieuDai = scanner.nextDouble();
+
+		        System.out.print("Nhập chiều rộng: ");
+		        double chieuRong = scanner.nextDouble();
+
+	
+		    }
+		    
+	 	}
+	}
+}
+
+
