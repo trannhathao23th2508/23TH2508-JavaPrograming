259065e96f1db2524175fb8f1e5cf409026e8fd5
trannhathao23th2508 <haotranvipqwe15@gmail.com>
2024-09-26 22:38:03 +0700

    .

---
+++ Bai4/src/Haotn/BaiTap/TinhDelta.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Haotn.BaiTap;
+
+/**
+ * 
+ */
+public class TinhDelta {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
