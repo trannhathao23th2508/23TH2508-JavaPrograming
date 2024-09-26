b0a51ced93db4efd158844cd2f1c285bed136ba6
trannhathao23th2508 <haotranvipqwe15@gmail.com>
2024-09-26 22:33:53 +0700

    hoàn thành

--- bai3/src/bai3/ThetichHCN.java
+++ bai3/src/bai3/ThetichHCN.java
@@ -3,6 +3,8 @@
  */
 package bai3;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -11,9 +13,24 @@ public class ThetichHCN {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
+		        Scanner scanner = new Scanner(System.in);
 
+		        // Nhập cạnh của khối lập phương
+		        System.out.print("Nhập cạnh của khối lập phương: ");
+		        double canh = scanner.nextDouble();
+
+		        // Tính thể tích
+		        double theTich = canh * canh * canh;
+
+		        // Xuất kết quả
+		        System.out.println("Thể tích của khối lập phương là: " + theTich);
 	}
-
+	
 }
+	
+	
+	
+
+
