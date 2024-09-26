7ed8b799f50b133f6cbe0675d41962a08fe4c54f
trannhathao23th2508 <haotranvipqwe15@gmail.com>
2024-09-26 22:43:25 +0700

    Update TinhDelta.java

--- Bai4/src/Haotn/BaiTap/TinhDelta.java
+++ Bai4/src/Haotn/BaiTap/TinhDelta.java
@@ -3,6 +3,8 @@
  */
 package Haotn.BaiTap;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +15,22 @@ public class TinhDelta {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in); 
+		 
+			System.out.print("Nhập hệ số a: ");
+	        double a = scanner.nextDouble();
 
+	        System.out.print(" Nhập hệ số b: ");
+	        double b = scanner.nextDouble(); 
+	        
+	        System.out.print("Nhập hệ số c: ");
+	        double c = scanner.nextDouble();
+//tính delta
+	        double delta = b * b - 4 * a * c;
+	        
+	        
+	        System.out.println("Delta = " + delta);
+	        System.out.println("Căn bậc hai của delta là: " + Math.sqrt(delta));
 	}
 
 }
