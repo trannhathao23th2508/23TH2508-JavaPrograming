ad1d545a63eb9b54824864ecc183fda3ffe97f38
trannhathao23th2508 <haotranvipqwe15@gmail.com>
2024-09-27 10:02:50 +0700

    nhap a  b c
    đang làm

---
+++ Baitap2Lab2/src/Haotn/BaiTap/TinhPhuongTrinhBacHai.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Haotn.BaiTap;
+
+/**
+ * 
+ */
+public class TinhPhuongTrinhBacHai {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
